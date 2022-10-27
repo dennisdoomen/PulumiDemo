bf35d93e8b808449147fa5da0fa2bc1bb9630cab
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2022-10-27 21:02:22 +0200

    Push docker images to AWS ECR

--- src/Deploy/Program.cs
+++ src/Deploy/Program.cs
@@ -1,15 +1,51 @@
-﻿using Pulumi;
-using Pulumi.Aws.S3;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pulumi;
+using Pulumi.Aws.Ecr;
+using Pulumi.Docker;
 
-return await Deployment.RunAsync(() =>
+return await Deployment.RunAsync(async () =>
 {
-   // Create an AWS resource (S3 Bucket)
-   var bucket = new Bucket("my-bucket");
+    string rootDirectory = Environment.GetEnvironmentVariable("Root");
+    string configuration = Environment.GetEnvironmentVariable("Configuration");
 
-   // Export the name of the bucket
-   return new Dictionary<string, object?>
-   {
-      ["bucketName"] = bucket.Id
-   };
+    var ecr = new Repository("minimalapi", new RepositoryArgs());
+
+    var credentials = GetCredentials.Invoke(new GetCredentialsInvokeArgs
+    {
+        RegistryId = ecr.RegistryId
+    });
+
+    var decodedCredentials = credentials.Apply(o => Encoding.UTF8.GetString(
+        Convert.FromBase64String(o.AuthorizationToken)));
+
+    var registry = new ImageRegistry
+    {
+        Username = decodedCredentials.Apply(c => c.Split(":").First()),
+        Password = decodedCredentials.Apply(c => c.Split(":").Last()),
+        Server = credentials.Apply(c => c.ProxyEndpoint)
+    };
+
+    var image = new Image("minimalapi", new ImageArgs
+    {
+        Build = new DockerBuild
+        {
+            Context = rootDirectory,
+            Dockerfile = $"{rootDirectory}/minimalapi.dockerfile",
+            Args = new Dictionary<string, string>
+            {
+                ["CONFIGURATION"] = configuration
+            }
+        },
+        ImageName = Output.Format($"{ecr.RepositoryUrl}:v1.0.0"),
+        LocalImageName = "minimalapi:v1.0.0",
+        Registry = registry
+    });
+
+    return new Dictionary<string, object?>
+    {
+        ["ImageName"] = image.ImageName
+    };
 });
